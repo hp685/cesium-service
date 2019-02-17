34acb5c8b1a8f4db239812a57ac213d269850a21
hp685 <harsh@nyu.edu>
2019-02-16 19:18:10 -0800

    updates to components, actions and reducers

--- csmodal/src/index.js
+++ csmodal/src/index.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import './index.css';
-import CesiumServiceWidget from './App';
+import VisibleCesiumServiceWidget from './App';
 import * as serviceWorker from './serviceWorker';
-import cesiumApp from './reducers';
-import {chooseService, setField} from './actions';
+import store from './store';
 
-const store = createStore(cesiumApp);
+
 ReactDOM.render(
     <Provider store={store}>
-        <CesiumServiceWidget />
+        <VisibleCesiumServiceWidget />
     </Provider>,
     document.getElementById('root')
 );
 
 console.log(store.getState());
-
-const unsubscribe = store.subscribe( () => console.log(store.getState()));
-
-store.dispatch(chooseService('Areacheck'));
-store.dispatch(setField('serial number', 'sn123'));
-
-unsubscribe();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- csmodal/src/reducers.js
+++ csmodal/src/reducers.js
@@ -1,41 +1,46 @@
-import { CHOOSE_SERVICE, SET_FIELD, SET_RESPONSE } from './actions';
+import {
+  CHOOSE_SERVICE,
+  SET_FIELD,
+  SET_RESPONSE,
+  SET_SERVICES,
+  RESET_FORM,
+  } from './actions';
 import { combineReducers } from 'redux';
+import { defaultState } from './data';
+import { zipObject, get }  from 'lodash';
+import {createReducer} from 'redux-starter-kit';
+import store from './store';
+const cesiumApp = createReducer(defaultState, {
 
-const chooseServiceReducer = (state=[], action) => {
+  set_services: (state, action) => {
+    state.services = action.service;
+  },
 
-    switch(action.type){
-        case CHOOSE_SERVICE:
-            return Object.assign({}, state, {
-                selectedService: action.service,
-            });
-        default:
-            return state;
+  set_field: (state, action) => {
+    console.log('setting field', state.selectedService, action.key, action.value)
+    //state.schema[state.selectedService][action.key] = action.value
+    let schema = state.schema
+    let serviceFields = {}
+    serviceFields[action.key] = action.value
+    let newFields = {...state.schema[state.selectedService], ...serviceFields}
+    state.schema[state.selectedService] = newFields
 
-    } 
-}
 
-const setFieldReducer = (fields={}, action) => {
-    switch(action.type){
-        case SET_FIELD:
-            return {...fields, ...action.field}
-        default:
-            return fields;
-    }
-}
+  },
 
-const setResponseReducer = (response={}, action) => {
-    switch(action.type){
-        case SET_RESPONSE:
-            return {...response}
-        default:
-            return response;
-    }
-}
+  set_response: (state, action) => {
 
-const cesiumApp = () => combineReducers(
-    chooseServiceReducer,
-    setFieldReducer,
-    setResponseReducer,
-)
+  },
+
+  choose_service: (state, action) => {
+    state.selectedService = action.service;
+  },
+
+  reset_form: (state, action) => {
+    state.schema[state.selectedService] = {};
+    state.clearForm = true
+  }
+
+})
 
 export default cesiumApp;
