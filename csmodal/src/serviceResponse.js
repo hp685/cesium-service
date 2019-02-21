fa21285739285e1c5506a9be117a1561819b5036
burs <burs@cisco.com>
2019-02-20 16:24:29 -0800

    Add response tabs

--- csmodal/src/serviceResponse.js
+++ csmodal/src/serviceResponse.js
@@ -1,49 +1,83 @@
 import React from 'react';
 import { JsonToTable } from 'react-json-to-table';
-import {Message, Label, Form} from 'semantic-ui-react';
+import {Message, Label, Form, Tab, List} from 'semantic-ui-react';
+
+require("./index.css");
 
 class ServiceResponse extends React.Component{
     constructor(props){
         super(props);
         this.state = {
         }
-
     }
 
     componentWillMount = () => {
     }
 
-    displayResponse = (response) => {
-        let rows = []
-        rows.push(
-          <Form.Field>
-            <Label color="grey">
-              Response Code: {response["code"]}
-            </Label>
-          </Form.Field>
-        );
-        rows.push(
-          <Form.Field>
-            <Label color="grey">
-              Data: {Object.values(response["results"])}
-            </Label>
-          </Form.Field>);
-        return rows;
+    displayServiceResponse = (response) => {
+        return(<div>
+            {
+              Object.keys(response).map((key, index) => ( 
+                <p key={index}><b>{key}</b>:{response[key]}</p> 
+              ))
+            }
+          </div>)
     }
+
+    displayResponse = (response) => { 
+
+        console.log('props', response["code"]);
+        
+        if (response["code"] === 'OK'){
+            return(
+            <div>
+              <p> {this.displayServiceResponse(response.results[0])}</p>
+            </div>
+        );}
+
+        if (response["code"] !== 'OK')
+        {   
+            const response_pane = [{ menuItem: 'Response', pane: (
+                <Tab.Pane key='tab4'>          
+                  <List>
+                    <List.Item>Status: Fail</List.Item>
+                    <List.Item>error_code: {response.code}</List.Item>
+                    <List.Item>error_message: {response.error_message}</List.Item>
+                  </List>
+                </Tab.Pane>
+              )},
+            {menuItem: 'Details', pane: (                
+            <Tab.Pane key='tab4'>          
+            {this.displayServiceResponse(response.debug_info[0])}
+          </Tab.Pane>)}]
+            return (<div><Tab panes={response_pane} renderActiveOnly={false} /></div>);
+        }   
+        
+    }
+
     render(){
+        let result, color;
+        if (this.props.response["code"] === 'OK'){
+            result = 'PASS'; color = "green";
+            } 
+        else {
+            result = 'FAIL'; color = "red";
+        }
         return (
         <div>
             <Message>
-                <Label color="green">
-                    Response:
+                <Label color={color}>
+                    Result: {result}
                 </Label>
             <p>
-                {JSON.stringify(this.props.response)}
+                {this.displayResponse(this.props.response)}
             </p>
             </Message>
         </div>
         );
     }
 }
+
+
 
 export default ServiceResponse;
